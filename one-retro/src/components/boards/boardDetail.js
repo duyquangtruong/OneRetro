0ac63f50a17031e82d5365ed7068660f1e53f82d
Trương Quang Duy <truongquangduy343@gmail.com>
2020-11-18 14:25:50 +0700

    create UI

--- one-retro/src/components/boards/boardDetail.js
+++ one-retro/src/components/boards/boardDetail.js
@@ -2,7 +2,14 @@ import Header from "../header/header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./boardDetail.css";
 import AddButton from "../../images/add.png";
-import { Container, Row, Col, Card, Table } from "react-bootstrap";
+import EditButton from "../../images/edit.png";
+import { Row, Col, Card, Accordion, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+const GET_ALL_CARD_API = "http://localhost:3001/boards/detail";
+const CARD_TYPE_WENTWELL = 0;
+const CARD_TYPE_TOIMPROVE = 1;
+const CARD_TYPE_ACTIONITEMS = 2;
+const NUMBER_OF_COLUMN = 3;
 
 function BoardDetail() {
   return (
@@ -16,6 +23,93 @@ function BoardDetail() {
 }
 
 function CardList() {
+  const [wentWellCards, setWentWellCards] = useState([{}]);
+  const [toImproveCards, setToImproveCards] = useState([{}]);
+  const [actionItemsCards, setActionItemsCards] = useState([{}]);
+
+  useEffect(() => {
+    fetch(GET_ALL_CARD_API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setWentWellCards(res.wentWellCards);
+        setToImproveCards(res.toImproveCards);
+        setActionItemsCards(res.actionItemsCards);
+      });
+  }, []);
+
+  const wentWellList = wentWellCards.map((card, index) => {
+    return (
+      <Row className="justify-content-center mb-3">
+        {" "}
+        <Card body style={{ width: "95%" }}>
+          <Row>
+            <Col sm={11}>{card.content}</Col>
+            <Col sm={1}>
+              <img src={EditButton} style={{ width: "10px" }} />
+            </Col>
+          </Row>
+        </Card>
+      </Row>
+    );
+  });
+
+  const toImpoveList = toImproveCards.map((card, index) => {
+    return (
+      <Row className="justify-content-center mb-3">
+        {" "}
+        <Card body style={{ width: "95%" }}>
+          <Row>
+            <Col sm={11}>{card.content}</Col>
+            <Col sm={1}>
+              <img src={EditButton} style={{ width: "10px" }} />
+            </Col>
+          </Row>
+        </Card>
+      </Row>
+    );
+  });
+
+  const actionItemsList = actionItemsCards.map((card, index) => {
+    return (
+      <Row className="justify-content-center mb-3">
+        {" "}
+        <Card body style={{ width: "95%" }}>
+          <Row>
+            <Col sm={11}>{card.content}</Col>
+            <Col sm={1}>
+              <img src={EditButton} style={{ width: "10px" }} />
+            </Col>
+          </Row>
+        </Card>
+      </Row>
+    );
+  });
+
+  function handleAddCard(event) {
+    const cardType = event.currentTarget.id;
+  }
+
+  let addButtons = [];
+  let i;
+  for (i = 0; i < NUMBER_OF_COLUMN; i++) {
+    addButtons.push(
+      <Col className="addButton">
+        <img
+          src={AddButton}
+          className="addImage"
+          id={i}
+          onClick={handleAddCard}
+        />
+      </Col>
+    );
+  }
+
   return (
     <div>
       <Row>
@@ -32,78 +126,113 @@ function CardList() {
 
       <Row>
         <Col className="addButton">
-          <img src={AddButton} style={{ width: "8%" }} />
+          <img src={AddButton} className="addImage" />
         </Col>
         <Col className="addButton">
-          <img src={AddButton} width="8%" />
+          <img src={AddButton} className="addImage" />
         </Col>
         <Col className="addButton">
-          <img src={AddButton} width="8%" />
+          <img src={AddButton} className="addImage" />
         </Col>
       </Row>
 
       <Row>
         <Col>
-          <Row className="justify-content-center">
+          <Row className="justify-content-center mb-3">
             {" "}
-            <Card body style={{ width: "95%" }}>
-              {" "}
-              row1
-            </Card>
-          </Row>
-          <Row className="justify-content-center">
-            {" "}
-            <Card body style={{ width: "95%" }}>
-              {" "}
-              row2
-            </Card>
-          </Row>
-          <Row className="justify-content-center">
-            {" "}
-            <Card body style={{ width: "95%" }}>
-              {" "}
-              row3
+            <Card body style={{ width: "95%", height: "auto" }}>
+              <Row>
+                <Col sm={11}>
+                  <Row>
+                    <textarea
+                      className="contentCard"
+                      onInput={(event) => {
+                        event.currentTarget.style.height =
+                          event.currentTarget.scrollHeight + "px";
+                      }}
+                    >
+                      askjdhaskdhawiudhuiwahdiuawdhaiuwhdiawhdiuaw asdas d as da
+                      hdiuawhdihawdiuhaiwdhwiaudhaiwudhiuawhdiuwahdiuawhdiuahwdiuhawiudhawidhaiwdhai
+                    </textarea>
+                  </Row>
+                </Col>
+                <Col sm={1}>
+                  <img src={EditButton} className="editButton" />
+                </Col>
+              </Row>
+              <Row hidden={true} className="mt-2">
+                <Button className="ml-2">Done</Button>
+                <Button variant="danger" className="ml-auto mr-2">
+                  Delete
+                </Button>
+              </Row>
             </Card>
           </Row>
         </Col>
         <Col>
-          <Row className="justify-content-center">
+          {" "}
+          <Row className="justify-content-center mb-3">
             {" "}
-            <Card body style={{ width: "95%" }}>
-              {" "}
-              row1
-            </Card>
-          </Row>
-          <Row className="justify-content-center">
-            {" "}
-            <Card body style={{ width: "95%" }}>
-              {" "}
-              row2
-            </Card>
-          </Row>
-          <Row className="justify-content-center">
-            {" "}
-            <Card body style={{ width: "95%" }}>
-              {" "}
-              row3
+            <Card body style={{ width: "95%", height: "auto" }}>
+              <Row>
+                <Col sm={11}>
+                  <Row>
+                    <textarea
+                      className="contentCard"
+                      onInput={(event) => {
+                        event.currentTarget.style.height =
+                          event.currentTarget.scrollHeight + "px";
+                      }}
+                    >
+                      askjdhaskdhawiudhuiwahdiuawdhaiuwhdiawhdiuaw asdas d as da
+                      hdiuawhdihawdiuhaiwdhwiaudhaiwudhiuawhdiuwahdiuawhdiuahwdiuhawiudhawidhaiwdhai
+                    </textarea>
+                  </Row>
+                </Col>
+                <Col sm={1}>
+                  <img src={EditButton} className="editButton" />
+                </Col>
+              </Row>
+              <Row hidden={false} className="mt-2">
+                <Button className="ml-2">Done</Button>
+                <Button variant="danger" className="ml-auto mr-2">
+                  Delete
+                </Button>
+              </Row>
             </Card>
           </Row>
         </Col>
-        <Col className="col-md">
-          <Row className="justify-content-center">
+        <Col>
+          {" "}
+          <Row className="justify-content-center mb-3">
             {" "}
-            <Card body> row1</Card>
-          </Row>
-          <Row className="justify-content-center">
-            {" "}
-            <Card body className="">
-              {" "}
-              row2
+            <Card body style={{ width: "95%", height: "auto" }}>
+              <Row>
+                <Col sm={11}>
+                  <Row>
+                    <textarea
+                      className="contentCard"
+                      onInput={(event) => {
+                        event.currentTarget.style.height =
+                          event.currentTarget.scrollHeight + "px";
+                      }}
+                    >
+                      askjdhaskdhawiudhuiwahdiuawdhaiuwhdiawhdiuaw asdas d as da
+                      hdiuawhdihawdiuhaiwdhwiaudhaiwudhiuawhdiuwahdiuawhdiuahwdiuhawiudhawidhaiwdhai
+                    </textarea>
+                  </Row>
+                </Col>
+                <Col sm={1}>
+                  <img src={EditButton} className="editButton" />
+                </Col>
+              </Row>
+              <Row hidden={false} className="mt-2">
+                <Button className="ml-2">Done</Button>
+                <Button variant="danger" className="ml-auto mr-2">
+                  Delete
+                </Button>
+              </Row>
             </Card>
-          </Row>
-          <Row className="justify-content-center">
-            {" "}
-            <Card body> row3</Card>
           </Row>
         </Col>
       </Row>
--- one-retro/src/index.js
+++ one-retro/src/index.js
@@ -18,7 +18,7 @@ function Index() {
       <div>
         <Route path="/login" exact component={Login} />
         <Route path="/boards/detail" exact component={BoardDetail} />
-        <Route path="/boards" exact component={Boards} />
+        <Route path="/boards" path="/" exact component={Boards} />
         <Route path="/users" exact component={Users} />
       </div>
     </Router>
